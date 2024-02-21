3d99c2be98163b31f7dd3790dafd4234dd88ad0c
Cam5210 <Caabernethy@davidson.edu>
2024-02-21 17:03:54 -0500

    Refactored the code to a main function

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,70 +1,83 @@
 from bs4 import BeautifulSoup
 
-# Read the HTML file
-with open('page1.html', 'r') as file:
-    html_content = file.read()
 
-# Parse the HTML content
-soup = BeautifulSoup(html_content, 'html.parser')
+def process_html_content(html_content, tags_to_remove, tags_to_change, new_tag):
+    """
+    Process HTML content to remove specified tags, change certain tags to a new tag,
+    and extract content and deleted content.
 
-# Initialize the lists to track content and deleted content
-content = []
-deleted_content = []
+    Parameters:
+    - html_content: The HTML content as a string.
+    - tags_to_remove: A list of tag names to remove from the HTML.
+    - tags_to_change: A list of tag names to change to a new tag.
+    - new_tag: The new tag name to replace tags_to_change with.
 
+    Returns:
+    - A tuple of two elements: final_content and deleted_final_content as strings.
+    """
+    soup = BeautifulSoup(html_content, 'html.parser')
+    content = []
+    deleted_content = []
 
-def remove_tag(soup, tag_names, deleted_content):
-    for tag_name in tag_names:
+    # Remove specified tags
+    for tag_name in tags_to_remove:
         for tag in soup.find_all(tag_name):
-            # Before decomposing, add a representation of the tag to deleted_content
             deleted_content.append('Deleted Tag: ' + str(tag))
             tag.decompose()
-    return soup
 
-
-# Specify the tags to remove
-tags_to_remove = ['link', 'style', 'script', 'meta',
-                  'path', 'img', 'logo', 'svg', 'noscript', 'iframe']
-soup = remove_tag(soup, tags_to_remove, deleted_content)
-
-
-# Change all sup and sub tags to p tags
-def change_tag(soup, tag_names, new_tag, deleted_content):
-    for tag_name in tag_names:
+    # Change specified tags to new_tag
+    for tag_name in tags_to_change:
         for tag in soup.find_all(tag_name):
-            # Before replacing the tag, add a representation of the tag to deleted_content
             deleted_content.append('Deleted Tag: ' + str(tag))
             tag.replace_with(soup.new_tag(new_tag, string=tag.text))
-    return soup
+
+    # Extract remaining content
+    for element in soup.recursiveChildGenerator():
+        if element.name == 'a':
+            href = element.get('href')
+            if href and not href.startswith('#'):
+                content.append('Link: ' + href)
+            else:
+                deleted_content.append(
+                    'Deleted Link: ' + (str(href) if href else ''))
+        elif element.name is None:
+            text = element.strip()
+            if text:
+                content.append('Text: ' + text)
+
+    # Prepare final content strings
+    final_content = '\n'.join([item for item in content if item.strip()])
+    deleted_final_content = '\n'.join(
+        [item for item in deleted_content if item.strip()])
+
+    return final_content, deleted_final_content
 
 
-soup = change_tag(soup, ['sup', 'sub'], 'p', deleted_content)
+def main(input_file_path, output_file_path, deleted_content_file_path):
 
-for element in soup.recursiveChildGenerator():
-    if element.name == 'a':
-        href = element.get('href')
-        if href and not href.startswith('#'):
-            content.append('Link: ' + href)
-        else:
-            # Add non-matching 'a' elements to deleted_content
-            deleted_content.append(
-                'Deleted Link: ' + str(href) if href else '')
-    elif element.name is None:
-        text = element.strip()
-        if text:
-            content.append('Text: ' + text)
-        else:
-            # Skip adding empty strings to deleted_content here as they were not actively removed
-            pass
+    # Read the HTML file
+    with open(input_file_path, 'r') as file:
+        html_content = file.read()
 
-# Filter out blank lines and join the content with newline characters
-final_content = '\n'.join([item for item in content if item.strip()])
-deleted_final_content = '\n'.join(
-    [item for item in deleted_content if item.strip()])
+    # Process the HTML content
+    tags_to_remove = ['link', 'style', 'script', 'meta',
+                      'path', 'img', 'logo', 'svg', 'noscript', 'iframe']
+    tags_to_change = ['sup', 'sub']
+    new_tag = 'p'
+    final_content, deleted_final_content = process_html_content(
+        html_content, tags_to_remove, tags_to_change, new_tag)
 
-# Save the main content to a file
-with open('output2.txt', 'w', encoding='utf-8') as output_file:
-    output_file.write(final_content)
+    # Save the main content to a file
+    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(final_content)
 
-# Save the deleted (screened) content to a separate file
-with open('deleted_content2.txt', 'w', encoding='utf-8') as deleted_file:
-    deleted_file.write(deleted_final_content)
+    # Save the deleted (screened) content to a separate file
+    with open(deleted_content_file_path, 'w', encoding='utf-8') as deleted_file:
+        deleted_file.write(deleted_final_content)
+
+# Example Usage of Main
+# main('page1.html', 'output.txt', 'deleted_output.txt')
+
+
+if __name__ == '__main__':
+    main()
